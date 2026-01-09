59e7a155610c84dace1b962f0f164bb541443547
abeertech01 <abeer.technology@gmail.com>
2026-01-09 17:02:16 +0600

    @svgr/webpack configuration and some other stuffs

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,42 @@
+import memojiImage from "@/assets/images/memoji-computer.png"
+import Image from "next/image"
+import ArrowDown from "@/assets/icons/arrow-down.svg"
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
-};
+  return (
+    <div className="py-32">
+      <div className="container">
+        <div className="flex flex-col items-center">
+          <Image
+            src={memojiImage}
+            className="size-25"
+            alt="Person peeking from behind laptop"
+          />
+          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="text-sm font-medium">
+              Available for new projects
+            </div>
+          </div>
+        </div>
+        <h1 className="font-serif text-3xl text-center mt-8">
+          Building Exceptional User Experiences
+        </h1>
+        <p>
+          I specialize in transforming designs into functional, high-performing
+          web applications. Let&apos;s discuss your text project.
+        </p>
+        <div>
+          <button>
+            <span>Explore My Work</span>
+            <ArrowDown />
+          </button>
+          <button>
+            <span>👋</span>
+            <span>Let&apos;s Connect</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
