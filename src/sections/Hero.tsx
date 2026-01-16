f2d559b86aecf6392c1444784449ab88d3b4b631
abeertech01 <abeer.technology@gmail.com>
2026-01-17 03:25:28 +0600

    hero section particles developed

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,10 +1,32 @@
 import memojiImage from "@/assets/images/memoji-computer.png"
 import Image from "next/image"
 import ArrowDown from "@/assets/icons/arrow-down.svg"
+import grainImage from "@/assets/images/grain.jpg"
+import StarIcon from "@/assets/icons/star.svg"
+import HeroOrbit from "@/components/HeroOrbit"
 
 export const HeroSection = () => {
   return (
-    <div className="py-32">
+    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+      <div
+        className="absolute inset-0 -z-30 opacity-5"
+        style={{
+          backgroundImage: `url(${grainImage.src})`,
+        }}
+      ></div>
+      <div className="size-155 hero-ring"></div>
+      <div className="size-205 hero-ring"></div>
+      <div className="size-255 hero-ring"></div>
+      <div className="size-305 hero-ring"></div>
+      <HeroOrbit size={800} rotation={-72}>
+        <StarIcon className="size-28 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={20}>
+        <StarIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={590} rotation={98}>
+        <StarIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
       <div className="container">
         <div className="flex flex-col items-center">
           <Image
@@ -19,21 +41,24 @@ export const HeroSection = () => {
             </div>
           </div>
         </div>
-        <h1 className="font-serif text-3xl text-center mt-8">
-          Building Exceptional User Experiences
-        </h1>
-        <p>
-          I specialize in transforming designs into functional, high-performing
-          web applications. Let&apos;s discuss your text project.
-        </p>
-        <div>
-          <button>
-            <span>Explore My Work</span>
-            <ArrowDown />
+        <div className="max-w-lg mx-auto">
+          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+            Building Exceptional User Experiences
+          </h1>
+          <p className="mt-4 text-center text-white/60 md:text-lg">
+            I specialize in transforming designs into functional,
+            high-performing web applications. Let&apos;s discuss your text
+            project.
+          </p>
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <span className="font-semibold">Explore My Work</span>
+            <ArrowDown className="size-4" />
           </button>
-          <button>
+          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
             <span>👋</span>
-            <span>Let&apos;s Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
