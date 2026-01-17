c6c726da068b845be968efdc1d32320c4700015a
abeertech01 <abeer.technology@gmail.com>
2026-01-18 05:40:16 +0600

    featured projects section

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,6 +1,10 @@
-import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png";
-import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png";
-import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png";
+import darkSaasLandingPage from "@/assets/images/dark-saas-landing-page.png"
+import lightSaasLandingPage from "@/assets/images/light-saas-landing-page.png"
+import aiStartupLandingPage from "@/assets/images/ai-startup-landing-page.png"
+import CheckCircleIcon from "@/assets/icons/check-circle.svg"
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+import grainImage from "@/assets/images/grain.jpg"
+import Image from "next/image"
 
 const portfolioProjects = [
   {
@@ -39,8 +43,77 @@ const portfolioProjects = [
     link: "https://youtu.be/Z7I5uSRHMHg",
     image: aiStartupLandingPage,
   },
-];
+]
 
 export const ProjectsSection = () => {
-  return <div>Projects Section</div>;
-};
+  return (
+    <section className="lg:py-24 pb-16">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="bg-clip-text bg-linear-to-r from-emerald-300 to-sky-400 font-semibold text-transparent uppercase tracking-widest">
+            Real-World Results
+          </p>
+        </div>
+        <h2 className="mt-6 font-serif text-3xl md:text-5xl text-center">
+          Featured Projects
+        </h2>
+        <p className="mx-auto mt-4 max-w-md text-white/60 md:text-lg lg:text-lg text-center">
+          See how I transformed concepts into engaging digital experiences.
+        </p>
+        <div className="flex flex-col gap-20 mt-10 md:mt-20">
+          {portfolioProjects.map((project) => (
+            <div
+              key={project.title}
+              className="z-0 after:z-10 after:absolute relative after:inset-0 bg-gray-800 px-8 md:px-10 lg:px-20 pt-8 md:pt-12 lg:pt-16 rounded-3xl after:rounded-3xl after:outline-2 after:outline-white/20 after:-outline-offset-2 overflow-hidden after:content-[''] after:pointer-events-none"
+            >
+              <div
+                className="-z-10 absolute inset-0 opacity-5"
+                style={{
+                  backgroundImage: `url(${grainImage.src})`,
+                }}
+              ></div>
+              <div className="lg:gap-16 lg:grid lg:grid-cols-2">
+                <div className="lg:pb-16">
+                  <div className="inline-flex gap-2 bg-clip-text bg-linear-to-r from-emerald-300 to-sky-400 font-bold text-transparent text-sm uppercase tracking-widest">
+                    <span>{project.company}</span>
+                    <span>&bull;</span>
+                    <span>{project.year}</span>
+                  </div>
+
+                  <h3 className="mt-2 md:mt-5 font-serif text-2xl md:text-4xl">
+                    {project.title}
+                  </h3>
+                  <hr className="mt-4 md:mt-5 border-white/5 border-t-2" />
+                  <ul className="flex flex-col gap-4 mt-4 md:mt-5">
+                    {project.results.map((result, resultIndex) => (
+                      <li
+                        key={resultIndex}
+                        className="flex gap-2 text-white/50 text-sm md:text-base"
+                      >
+                        <CheckCircleIcon className="size-5 md:size-6" />
+                        <span>{result.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                  <a href={project.link}>
+                    <button className="inline-flex justify-center items-center gap-2 bg-white mt-8 px-6 rounded-xl w-full md:w-auto h-12 font-semibold text-gray-950">
+                      <span>Visit Live Site</span>
+                      <ArrowUpRightIcon />
+                    </button>
+                  </a>
+                </div>
+                <div className="relative">
+                  <Image
+                    src={project.image}
+                    alt={project.title}
+                    className="lg:absolute mt-8 lg:mt-0 -mb-4 md:mb-0 lg:w-auto lg:max-w-none lg:h-full"
+                  />
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
