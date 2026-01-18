3840a84e64cd775f3763b9c8bb93fa96f81c665b
abeertech01 <abeer.technology@gmail.com>
2026-01-18 21:21:03 +0600

    tape section design developed

--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -1,3 +1,36 @@
+import StarIcon from "@/assets/icons/star.svg"
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Secure",
+  "Interactive",
+  "Scalable",
+  "User Friendly",
+  "Responsive",
+  "Maintainable",
+  "Search Optimized",
+  "Usable",
+  "Reliable",
+]
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
-};
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-linear-to-r from-emerald-300 to-sky-400 -mx-1 -rotate-3">
+        <div className="flex mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word) => (
+              <div key={word} className="inline-flex items-center gap-4">
+                <span className="font-extrabold text-gray-900 text-sm uppercase">
+                  {word}
+                </span>
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
