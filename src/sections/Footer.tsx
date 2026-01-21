0278c650b9e4bc2b6a52c72f58a34566ebcf537f
abeertech01 <abeer.technology@gmail.com>
2026-01-21 14:38:03 +0600

    hero section stars and sparkles animation

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,47 @@
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+
+const footerLinks = [
+  {
+    title: "YouTube",
+    href: "#",
+  },
+  {
+    title: "Twitter",
+    href: "#",
+  },
+  {
+    title: "Instagram",
+    href: "#",
+  },
+  {
+    title: "Linkedin",
+    href: "#",
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
-};
+  return (
+    <footer className="-z-10 relative overflow-clip">
+      <div className="bottom-0 left-1/2 -z-10 absolute bg-emerald-300/30 w-400 h-100 -translate-x-1/2 mask-[radial-gradient(50%_50%_at_bottom_center,black,transparent)]"></div>
+      <div className="container">
+        <div className="flex md:flex-row flex-col md:justify-between items-center gap-8 py-6 border-white/15 border-t text-sm">
+          <div className="text-white/40">
+            &copy; 2024. All the rights reserved.
+          </div>
+          <nav className="flex md:flex-row flex-col items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                href="#"
+                key={link.title}
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.title}</span>
+                <ArrowUpRightIcon />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,48 +8,96 @@ import HeroOrbit from "@/components/HeroOrbit"
 
 export const HeroSection = () => {
   return (
-    <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="z-0 relative py-32 md:py-48 lg:py-60 overflow-x-clip">
       <div className="absolute inset-0 mask-[linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]">
         <div
-          className="absolute inset-0 -z-30 opacity-5"
+          className="-z-30 absolute inset-0 opacity-5"
           style={{
             backgroundImage: `url(${grainImage.src})`,
           }}
         ></div>
-        <div className="size-155 hero-ring"></div>
-        <div className="size-205 hero-ring"></div>
-        <div className="size-255 hero-ring"></div>
-        <div className="size-305 hero-ring"></div>
-        <HeroOrbit size={800} rotation={-72}>
-          <StarIcon className="size-28 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
-          <StarIcon className="size-12 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit size={590} rotation={98}>
-          <StarIcon className="size-8 text-emerald-300" />
-        </HeroOrbit>
-        <HeroOrbit size={430} rotation={-14}>
+        <div className="hero-ring size-155"></div>
+        <div className="hero-ring size-205"></div>
+        <div className="hero-ring size-255"></div>
+        <div className="hero-ring size-305"></div>
+        <HeroOrbit
+          size={430}
+          rotation={-14}
+          shouldOrbit
+          orbitDuration="30s"
+          shouldSpin
+          spinDuration="3s"
+        >
           <SparkleIcon className="size-8 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={440} rotation={79}>
+        <HeroOrbit
+          size={440}
+          rotation={79}
+          shouldOrbit
+          orbitDuration="32s"
+          shouldSpin
+          spinDuration="3s"
+        >
           <SparkleIcon className="size-5 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={530} rotation={178}>
+        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
+          <div className="bg-emerald-300/20 rounded-full size-2"></div>
+        </HeroOrbit>
+        <HeroOrbit
+          size={530}
+          rotation={178}
+          shouldOrbit
+          orbitDuration="36s"
+          shouldSpin
+          spinDuration="3s"
+        >
           <SparkleIcon className="size-10 text-emerald-300/20" />
         </HeroOrbit>
-        <HeroOrbit size={710} rotation={144}>
+        <HeroOrbit
+          size={550}
+          rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-12 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={590}
+          rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-8 text-emerald-300" />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
+          <div className="bg-emerald-300/20 rounded-full size-2"></div>
+        </HeroOrbit>
+        <HeroOrbit
+          size={710}
+          rotation={144}
+          shouldOrbit
+          orbitDuration="44s"
+          shouldSpin
+          spinDuration="3s"
+        >
           <SparkleIcon className="size-14 text-emerald-300/20" />
         </HeroOrbit>
-        {/* circles instead of sparkles or stars */}
-        <HeroOrbit size={720} rotation={85}>
-          <div className="size-3 rounded-full bg-emerald-300/20"></div>
+        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
+          <div className="bg-emerald-300/20 rounded-full size-3"></div>
         </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41}>
-          <div className="size-2 rounded-full bg-emerald-300/20"></div>
-        </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5}>
-          <div className="size-2 rounded-full bg-emerald-300/20"></div>
+        <HeroOrbit
+          size={800}
+          rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-28 text-emerald-300" />
         </HeroOrbit>
       </div>
       <div className="container">
@@ -59,29 +107,31 @@ export const HeroSection = () => {
             className="size-25"
             alt="Person peeking from behind laptop"
           />
-          <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
-            <div className="text-sm font-medium">
+          <div className="inline-flex items-center gap-4 bg-gray-950 px-4 py-1.5 border border-gray-800 rounded-lg">
+            <div className="relative bg-green-500 rounded-full size-2.5">
+              <div className="absolute inset-0 bg-green-500 rounded-full animate-ping-large"></div>
+            </div>
+            <div className="font-medium text-sm">
               Available for new projects
             </div>
           </div>
         </div>
-        <div className="max-w-lg mx-auto">
-          <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">
+        <div className="mx-auto max-w-lg">
+          <h1 className="mt-8 font-serif text-3xl md:text-5xl text-center tracking-wide">
             Building Exceptional User Experiences
           </h1>
-          <p className="mt-4 text-center text-white/60 md:text-lg">
+          <p className="mt-4 text-white/60 md:text-lg text-center">
             I specialize in transforming designs into functional,
             high-performing web applications. Let&apos;s discuss your text
             project.
           </p>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-          <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+        <div className="flex md:flex-row flex-col justify-center items-center gap-4 mt-8">
+          <button className="inline-flex items-center gap-2 px-6 border border-white/15 rounded-xl h-12">
             <span className="font-semibold">Explore My Work</span>
             <ArrowDown className="size-4" />
           </button>
-          <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <button className="inline-flex items-center gap-2 bg-white px-6 border border-white rounded-xl h-12 text-gray-900">
             <span>👋</span>
             <span className="font-semibold">Let&apos;s Connect</span>
           </button>
